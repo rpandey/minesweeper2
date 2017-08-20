7c34dc2c8ba03c6ac4d6e95a33d75a30b538debf
rohit.pandey <rohit.pandey@ihg.com>
2017-08-20 20:17:01 +0530

    Code cleaning, removed unnecessary code

--- app/view1/minesweeper.js
+++ app/view1/minesweeper.js
@@ -21,7 +21,6 @@ angular.module('myApp.minesweeper', ['ngRoute'])
           max=5;
 
       var createMinefield = function() {
-
             minefield.rows = [];
 
             for(var i = 0; i < max; i++) {
@@ -34,23 +33,11 @@ angular.module('myApp.minesweeper', ['ngRoute'])
                 spot.isCovered = true;
                 row.spots.push(spot);
                 console.log(row.spots[j].content);
-
-               /* minefieldWithFlagCount.push({
-                  position:{
-                    row:0,
-                    column:0
-                  },
-                  count:0
-                })*/
-                //console.log("i: ",i, "j: ",j);
               }minefield.rows.push(row);
             }
-
-
-
-            //alert(minefield);
-            return minefield;
+          return minefield;
           },
+
           createRandomMine = function() {
             var row,
                 column,
@@ -59,21 +46,12 @@ angular.module('myApp.minesweeper', ['ngRoute'])
                 randomColumn=Math.round(Math.random()*max)-1;
             row = (randomRow==-1?0:randomRow);
             column = (randomColumn==-1?0:randomColumn);
-              //console.log("row: ",row,"randomRow:",randomRow,"column:",column,"randomColumn:",randomColumn);
             spot= getSpot(minefield, row, column);
             spot.content = "bomb";
-            //createFlags(row,column);
-            /*if(row !=0) {
-              spot.previous=row-1;
-            }
-            if(row != 3) {
-              spot.next=row+1;
-            }*/
-            // /console.log(spot);
           },
+
           getSpot =  function(minefield, row, column) {
             console.log("row: ",row,"column",column);
-           // if(row>0 && column>0 && column <3 && row <3){
                 var i,j,
                     rowMax=(row==max-1 ?max-1:row+1),
                     rowMin=(row==0 ?0:row-1),
@@ -86,24 +64,16 @@ angular.module('myApp.minesweeper', ['ngRoute'])
                         }
                     }
                 }
-
-            //}
             return minefield.rows[row].spots[column];
-          },
-          createFlags = function (row, column) {
+          };
+
+         /* createFlags = function (row, column) {
             var i,
                 j;
 
               for(i=(row==0 ?0:row-1);i< column +1 && i >0 && i<3; i++) {
 
                 for(j=column;j< row-1 && j >0 && j<3; j--) {
-
-                /*  if((minefieldWithFlagCount[i].position.row == i) && (minefieldWithFlagCount[i].position.column == j)){
-                    minefieldWithFlagCount[i].count++;
-                  }
-                  else {
-                    minefieldWithFlagCount[i].count=1;
-                  }*/
                   minefieldWithFlagCount.push({
                     position:{
                       row:i,
@@ -111,16 +81,12 @@ angular.module('myApp.minesweeper', ['ngRoute'])
                     },
                      count:0
                   });
-                  //console.log("i: ",i, "j: ",j);
                 }
               }
-
-          }
+          }*/
 
       $scope.minefield = createMinefield();
       for(var i=0;i<max;i++) {
         createRandomMine();
       }
-      console.log(minefieldWithFlagCount);
-      console.log("hello");
     }]);
