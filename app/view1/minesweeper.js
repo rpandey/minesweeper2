6d7c5665c164fb7bd2c46626c61631a45b2c963e
rohit.pandey <rohit.pandey@ihg.com>
2017-08-23 20:52:24 +0530

    Level of difficulty added

--- app/view1/minesweeper.js
+++ app/view1/minesweeper.js
@@ -2,112 +2,140 @@
 
 angular.module('myApp.minesweeper', ['ngRoute'])
 
-    .config(['$routeProvider', function($routeProvider) {
-      $routeProvider.when('/minesweeper', {
-        templateUrl: 'view1/minesweeper.html',
-        controller: 'MinesweeperController'
-      });
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/minesweeper', {
+            templateUrl: 'view1/minesweeper.html',
+            controller: 'MinesweeperController'
+        });
     }])
-    .controller('MinesweeperController', ['$scope', function($scope) {
-      var minefield = {},
-          hello,
-          minefieldWithFlagCount=[{
-            position:{
-              row:0,
-              column:0
-            },
-            count:0
-          }],
-          max=5,
-          flagPosBombCount=[];
+    .controller('MinesweeperController', ['$scope', function ($scope) {
+        var minefield = {},
+            minefieldWithFlagCount = [{
+                position: {
+                    row: 0,
+                    column: 0
+                },
+                count: 0
+            }],
+            levelOfDiff = {},
+            max = 3;
 
-      var createMinefield = function() {
-            minefield.rows = [];
+       // $scope.itemList = [];
+        $scope.options = ['easy', 'medium', 'hard'];
+        $scope.selectedLevel= 'easy';
+        //$scope.levels = [{id: "easy", name: "easy"}, {id: "medium", name: "medium"}, {id: "hard", name: "hard"}];
+        console.log("options",$scope.options,"levels:",$scope.levels);
 
-            for(var i = 0; i < max; i++) {
-              var row = {};
-              row.spots = [];
+        $scope.changedValue = function (name) {
 
-              for(var j = 0; j < max; j++) {
-                var spot = {};
-                spot.content = "empty";
-                spot.isCovered = true;
-                row.spots.push(spot);
-               // console.log(row.spots[j].content);
-              }minefield.rows.push(row);
+            if(!name) return false;
+            minefield.rows.length = 0;
+            $scope.init(name);
+        };
+
+        $scope.init = function (name) {
+            name = name || 'easy';
+            $scope.minefield = createMinefield();
+            generateMinesWithLevel(name);
+            for (var i = 0; i < max; i++) {
+                for (var j = 0; j < max; j++) {
+                    if (minefield.rows[i].places[j].content !== 'bomb') {
+                        getBombCount(minefield, i, j);
+                    }
+                }
             }
-          return minefield;
-          },
+        };
 
-          createRandomMine = function() {
-            var row,
-                column,
-                spot,
-                randomRow=Math.round(Math.random()*max)-1,
-                randomColumn=Math.round(Math.random()*max)-1;
-            row = (randomRow==-1?0:randomRow);
-            column = (randomColumn==-1?0:randomColumn);
-            spot= getSpot(minefield, row, column);
-            spot.content = "bomb";
-          },
+        var createMinefield = function () {
+                minefield.rows = [];
 
-          getSpot =  function(minefield, row, column) {
-            console.log("row: ",row,"column",column);
-                var i,j,
-                    rowMax=(row==max-1 ?max-1:row+1),
-                    rowMin=(row==0 ?0:row-1),
-                    colMin=(column==0?0:column-1),
-                    colMax=(column==max-1?max-1:column+1),
-                    countArr=[];
+                for (var i = 0; i < max; i++) {
+                    var row = {};
+                    row.places = [];
 
-              for(i=rowMin;i<rowMax+1;i++){
-                  var count=0,countBomb=0;
-                  for(j=colMin;j< colMax+1; j++){
-                      if(minefield.rows[i].spots[j].content=='empty'){
-                          minefield.rows[i].spots[j].content=count;
-                      }
-                  }
-              }
-            return minefield.rows[row].spots[column];
-          },
+                    for (var j = 0; j < max; j++) {
+                        var place = {};
+                        place.content = "empty";
+                        place.isCovered = true;
+                        row.places.push(place);
+                        // console.log(row.places[j].content);
+                    }
+                    minefield.rows.push(row);
+                }
+                return minefield;
+            },
 
-          getBombCount = function(minefield, row, column){
-              var rowMax=(row==max-1 ?max-1:row+1),
-                  rowMin=(row==0 ?0:row-1),
-                  colMin=(column==0?0:column-1),
-                  colMax=(column==max-1?max-1:column+1),
-                  i,
-                  j,
-                  count,
-                  countBomb=0;
+            createRandomMine = function () {
+                var row,
+                    column,
+                    place,
+                    randomRow = Math.round(Math.random() * max) - 1,
+                    randomColumn = Math.round(Math.random() * max) - 1;
+                row = (randomRow == -1 ? 0 : randomRow);
+                column = (randomColumn == -1 ? 0 : randomColumn);
+                place = getPlace(minefield, row, column);
+                place.content = "bomb";
+            },
 
-                for(i=rowMin;i<rowMax+1;i++){
-                    for(j=colMin;j< colMax+1; j++){
-                        if(minefield.rows[i].spots[j].content=='bomb'){
+            getPlace = function (minefield, row, column) {
+                console.log("row: ", row, "column", column);
+                var i, j,
+                    rowMax = (row == max - 1 ? max - 1 : row + 1),
+                    rowMin = (row == 0 ? 0 : row - 1),
+                    colMin = (column == 0 ? 0 : column - 1),
+                    colMax = (column == max - 1 ? max - 1 : column + 1),
+                    countArr = [];
+
+                for (i = rowMin; i < rowMax + 1; i++) {
+                    var count = 0, countBomb = 0;
+                    for (j = colMin; j < colMax + 1; j++) {
+                        if (minefield.rows[i].places[j].content == 'empty') {
+                            minefield.rows[i].places[j].content = count;
+                        }
+                    }
+                }
+                return minefield.rows[row].places[column];
+            },
+
+            getBombCount = function (minefield, row, column) {
+                var rowMax = (row == max - 1 ? max - 1 : row + 1),
+                    rowMin = (row == 0 ? 0 : row - 1),
+                    colMin = (column == 0 ? 0 : column - 1),
+                    colMax = (column == max - 1 ? max - 1 : column + 1),
+                    i,
+                    j,
+                    count,
+                    countBomb = 0;
+
+                for (i = rowMin; i < rowMax + 1; i++) {
+                    for (j = colMin; j < colMax + 1; j++) {
+                        if (minefield.rows[i].places[j].content == 'bomb') {
                             countBomb++;
                         }
                     }
                 }
-              minefield.rows[row].spots[column].content=countBomb;
-              console.log("minefield.rows["+row+"].spots["+column+"]: "+minefield.rows[row].spots[column].content);
-              flagPosBombCount.push({
-                  row:row,
-                  column:column,
-                  count:countBomb
-              });
-        };
+                minefield.rows[row].places[column].content = countBomb;
+                console.log("minefield.rows[" + row + "].places[" + column + "]: " + minefield.rows[row].places[column].content);
+            },
 
-      $scope.minefield = createMinefield();
-      for(var i=0;i<max+2;i++) {
-        createRandomMine();
-      }
-      console.log(flagPosBombCount);
-      for (var i=0;i<max;i++){
-          for(var j=0;j<max;j++) {
-              if(minefield.rows[i].spots[j].content!=='bomb'){
-                  getBombCount(minefield, i, j);
-              }
-          }
-      }
-        $scope.flagPosBombCount=flagPosBombCount;
+            generateMinesWithLevel = function (levelOfDiff) {
+                var maxLocal;
+                switch (levelOfDiff) {
+                    case 'easy':
+                        maxLocal = max;
+                        break;
+                    case 'medium':
+                        maxLocal = max + max / 2;
+                        break;
+                    case 'hard':
+                        maxLocal = max * 2;
+                        break;
+                    default:
+                        maxLocal = max;
+                }
+                for (var i = 0; i < maxLocal; i++) {
+                    createRandomMine();
+                }
+            };
+        $scope.init();
     }]);
